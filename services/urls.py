0e2d37f99830531f79e5589799d04957bbe8d22d
gonyango <grantononyango@gmail.com>
2024-12-22 17:34:31 +0300

    url path for services working with class based view

--- services/urls.py
+++ services/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('register/customer', views.register_customer, name='register_customer'),
     path('register/company/',views.register_company, name='register_company'),
+    path('services/', views.ServiceListView.as_view(), name='service_list')
 ]
