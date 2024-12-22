924e33a03d048ba27eea77adbe063900fca19865
gonyango <grantononyango@gmail.com>
2024-12-22 14:33:00 +0300

    created a companyregistration class form

--- services/forms.py
+++ services/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from .models import User, Customer
+from .models import User, Customer, ServiceField, Company
 
 class CustomerRegistrationForm(UserCreationForm):
     date_of_birth = forms.DateField(widget=forms.DateInput(attrs={'type':'date'}))
@@ -19,3 +19,23 @@ class CustomerRegistrationForm(UserCreationForm):
             date_of_birth = self.cleaned_data['date_of_birth']
         )
         return user
+    
+class CompanyRegistratioForm(UserCreationForm):
+    field_of_work = forms.ChoiceField(choices=ServiceField.choices)
+
+    class Meta:
+        model = User
+        fields = ['username','email','password1','password2']
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.is_company = True
+        if commit:
+            user.save()
+        Company.objects.create(
+            user = user,
+            field_of_work = self.cleaned_data['field_of_work']
+        )
+        return user
+    
+    
