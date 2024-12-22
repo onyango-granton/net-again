ae0771c5ea5783f8b4ff1adbb4944253e67a4e72
gonyango <grantononyango@gmail.com>
2024-12-22 17:47:35 +0300

    url to specific category passed as an url param to the view function used as a filter param

--- services/urls.py
+++ services/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('register/customer', views.register_customer, name='register_customer'),
     path('register/company/',views.register_company, name='register_company'),
     path('services/', views.ServiceListView.as_view(), name='service_list'),
-    path('services/<int:pk>/', views.ServiceDetailView.as_view(), name='service_detail')
+    path('services/<int:pk>/', views.ServiceDetailView.as_view(), name='service_detail'),
+    path('services/category/<str:field>/', views.ServiceCategoryListView.as_view(), name='service_category')
 ]
