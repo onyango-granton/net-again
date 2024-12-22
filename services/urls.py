c6ec04c4c65983e92ece2692c10a50eca6e62442
gonyango <grantononyango@gmail.com>
2024-12-22 17:33:53 +0300

    url path for register/company

--- services/urls.py
+++ services/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('register/customer', views.register_customer, name='register_customer'),
+    path('register/company/',views.register_company, name='register_company'),
 ]
