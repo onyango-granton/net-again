19f7e8b61e4d8fc91c4ed66d96273fc2a987c8b5
gonyango <grantononyango@gmail.com>
2024-12-22 15:37:02 +0300

    created register customer function that redirects to home page incase of success

--- services/views.py
+++ services/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CustomerRegistrationForm
+from django.contrib.auth import login
 
 # Create your views here.
+def register_customer(request):
+    if request.method == 'POST':
+        form = CustomerRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request,user)
+            return redirect('home')
+    else:
+        form = CustomerRegistrationForm()
+    return render(request, 'registration/register_customer.html', {'form':form})
