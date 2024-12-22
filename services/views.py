313f041ff1ebd361c0905a71b6386f740a3860ec
gonyango <grantononyango@gmail.com>
2024-12-22 15:43:26 +0300

    create a function to handle company registration

--- services/views.py
+++ services/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .forms import CustomerRegistrationForm
+from .forms import CustomerRegistrationForm, CompanyRegistratioForm
 from django.contrib.auth import login
 
 # Create your views here.
@@ -13,3 +13,14 @@ def register_customer(request):
     else:
         form = CustomerRegistrationForm()
     return render(request, 'registration/register_customer.html', {'form':form})
+
+def register_company(request):
+    if request.method == 'POST':
+        form = CompanyRegistratioForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = CompanyRegistratioForm()
+    return render(request, 'registration/register_company.html', {'form':form})
