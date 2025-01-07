f5f1bf27a56c6006f807de9f85443a052b6e499c
gonyango <grantononyango@gmail.com>
2025-01-07 15:38:11 +0300

    init add urls.py after reset

---
+++ users/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views as v
+
+urlpatterns = [
+    path('', v.register, name='register'),
+    path('company/', v.CompanySignUpView.as_view(), name='register_company'),
+    path('customer/', v.CustomerSignUpView.as_view(), name='register_customer'),
+    path('login/', v.LoginUserView, name='login_user')
+]
